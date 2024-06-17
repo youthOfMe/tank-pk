d344818ea10e8c018b99c0d32c3a61ce8161b742
youthOfMe <2040690180@qq.com>
2024-06-17 14:35:46 +0800

    feat: 添加键盘处理, 根据上下左右箭头的按键状态, 判断坦克的移动方向

--- src/main/java/com/yangge/tank/Main.java
+++ src/main/java/com/yangge/tank/Main.java
@@ -2,8 +2,12 @@ package com.yangge.tank;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
 
+        while (true) {
+            Thread.sleep(1);
+            tankFrame.repaint();
+        }
     }
 }
--- src/main/java/com/yangge/tank/TankFrame.java
+++ src/main/java/com/yangge/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.yangge.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -15,6 +17,8 @@ public class TankFrame extends Frame {
         this.setTitle("霍盛展科技有限公司管理系统");
         this.setVisible(true);
 
+        this.addKeyListener(new MyKeyListener());
+
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -26,8 +30,40 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics graphics) {
         graphics.fillRect(x, y, 50, 50);
-        x += 10;
-        y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key released");
+        }
     }
 
 }
